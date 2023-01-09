b5053a89b22f0bb971966cdf1f01003ba37f3d11
Albina Peposhi <apeposhi@2-B-3.42heilbronn.de>
2023-01-09 16:25:02 +0100

    added case function

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,11 +6,38 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:48:13 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/01/09 15:06:01 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/01/09 16:24:15 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+static int	ft_case(va_list v_list, const char c)
+{
+	unsigned long	ptr;
+
+	if (c == 'c')
+		return (ft_putchar(va_arg(v_list, int)));
+	else if (c == 's')
+		return (ft_putstr(va_arg(v_list, char *)));
+	else if (c == 'p')
+	{
+		ptr = va_arg(v_list, unsigned long);
+		if (ptr)
+			return (ft_puthexa_u(ptr, c));
+		return (ft_putstr("(nil"));
+	}
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr_fd(va_arg(v_list, int), 1));
+	else if (c == 'u')
+		return (ft_putunbr_base(va_arg(v_list, unsigned int), "0123456789"));
+	else if (c == 'x' || c == 'X')
+		return (ft_puthexa_u(va_arg(v_list, unsigned int), c));
+	else if (c == '%')
+		return (ft_putchar('%'));
+	else
+		return (-1);
+}
 
 int	ft_printf(const char *str, ...)
 {
@@ -20,14 +47,18 @@ int	ft_printf(const char *str, ...)
 
 	i = 0;
 	count = 0;
-	va_start(v_list,str);
-	// while(str[i])
-	// {
-	// 	if (str[i] = '%' && ft_strnstr("cspdiuxX%", str[i+1]))
-			
-	// 	else 
-	// 		write
-	// }
+	va_start(v_list, str);
+	while(str[i])
+	{
+		if (str[i] == '%' && ft_strchr("cspdiuxX%", s[i + 1]) != 0)
+		{
+			count += ft_case(v_list, s[i + 1]);
+			i++;
+		}
+		else 
+			count += ft_putchar(s[i]);
+		i++;
+	}
 	va_end(v_list);
 	return(0);
 }
